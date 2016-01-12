fd02bd3caf8b5be7932365fa7d19fcfa82f64668
Richard Stevens <r.j.a.m.stevens@utwente.nl>
2016-01-12 23:23:43 +0100

    Faster implementation of implicit routines. Results look identical to the previous implementation. Note however that more aggressive compiler flags result in a non-working code, which was not the case before. Is this possible a sign that something is wrong?

--- SolveImpEqnUpdate_YZ.F90
+++ SolveImpEqnUpdate_YZ.F90
@@ -17,8 +17,8 @@
      &      xstart(3)-lvlhalo:xend(3)+lvlhalo)
       real,intent(inout) :: rhs(1:nx,xstart(2):xend(2), &
      &      xstart(3):xend(3))
-      real, dimension(nx) :: amkl,apkl,ackl,fkl
-      integer :: jc,kc,info,ipkv(nxm),ic
+      real, dimension(nx) :: amkl,apkl,ackl
+      integer :: jc,kc,info,ipkv(nxm),ic,nrhs
       real :: betadx,ackl_b
       real :: amkT(nxm-1),ackT(nxm),apkT(nxm-1),appk(nx-3)
 
@@ -37,19 +37,24 @@
 
       call dgttrf(nxm,amkT,ackT,apkT,appk,ipkv,info)
 
-      do ic=xstart(3),xend(3)
-        do jc=xstart(2),xend(2)
-          do kc=1,nxm
-            ackl_b=1.0d0/(1.-ac3sk(kc)*betadx)
-            fkl(kc)=rhs(kc,jc,ic)*ackl_b
+      nrhs=(xend(3)-xstart(3)+1)*(xend(2)-xstart(2)+1)
+       do ic=xstart(3),xend(3)
+         do jc=xstart(2),xend(2)
+            do kc=1,nxm
+               ackl_b=1.0/(1.0-ac3sk(kc)*betadx)
+               rhs(kc,jc,ic)=rhs(kc,jc,ic)*ackl_b
+             end do
           end do
+      end do
 
-          call dgttrs('N',nxm,1,amkT,ackT,apkT,appk,ipkv,fkl,nxm,info)
+      call dgttrs('N',nxm,nrhs,amkT,ackT,apkT,appk,ipkv,rhs,nx,info)
 
-          do kc=1,nxm
-            q(kc,jc,ic)=q(kc,jc,ic) + fkl(kc)
+       do ic=xstart(3),xend(3)
+         do jc=xstart(2),xend(2)
+            do kc=1,nxm
+              q(kc,jc,ic)=q(kc,jc,ic) + rhs(kc,jc,ic)
+             end do
           end do
-        end do
       end do
 
       return
